3b9ffe7ae1dd133d4c876c3934507142eb0652ad
komkomh <komkomh@gmail.com>
2019-05-26 14:24:33 +0900

    Add No.345

---
+++ src/bin/no345.rs
@@ -0,0 +1,83 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        s: String,
+    }
+
+    let mut cww_len = -1;
+    let chars: Vec<char> = s.chars().collect();
+    for i in 0..chars.len() {
+        if chars[i] == 'c' {
+            let ww_len = get_ww_len(&chars, i);
+            if ww_len != -1 && (cww_len == -1 || ww_len < cww_len) {
+                cww_len = ww_len;
+            }
+        }
+    }
+    println!("{}", cww_len)
+}
+
+fn get_ww_len(chars: &Vec<char>, index: usize) -> isize {
+    let mut count = 0;
+    for i in index..chars.len() {
+        if chars[i] == 'w' {
+            count = count + 1;
+        }
+        if count == 2 {
+            return (i - index + 1) as isize;
+        }
+    }
+    return -1;
+}
